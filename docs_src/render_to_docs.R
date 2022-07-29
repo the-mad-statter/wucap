3fb7156930822b5f5c067a2bae51c780c3d8c160
Matthew Schuelke <matt@themadstatter.com>
2022-07-29 12:38:24 -0500

    basic site

---
+++ docs_src/render_to_docs.R
@@ -0,0 +1,13 @@
+render_to_docs <-
+  function(x,
+           output_format = "rmarkdown::html_document",
+           output_dir = "docs") {
+    rmarkdown::render(
+      x,
+      output_format = output_format,
+      output_dir = output_dir
+    )
+  }
+
+render_to_docs("docs_src/index.Rmd")
+render_to_docs("vignettes/redcap_project_migration_app.Rmd")
